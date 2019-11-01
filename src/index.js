1e8175d397620cafa61441b31c7f9c14df11834d
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-11-01 07:03:45 +0700

    Class based components and Refactoring from Functional to Class components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const App = () => {
+//     // get current location
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//     return <div>Hello World!</div>;
+// };
+
+class App extends React.Component {
+    // it is a must for you to define render method
+    render() {
+        // get current location
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+
+        return <div>Hello World!</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
